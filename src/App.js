e7b5795ede9d56ac0bc4ce6376552d167e2d1edc
Matt Raines <mraines4@gmail.com>
2019-04-29 14:31:16 -0400

    adds weather component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import Weather from './Weather';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        {'something'}
+        REACT DASHBOARD
+        <div className="widget">
+          <Weather />
+        </div>
       </div>
     );
 }
---
+++ src/Weather.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import axios from 'axios';
+
+class Weather extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            weather: {}
+        }
+    }
+
+    async componentDidMount() {
+        const weatherData = await axios.get('http://api.openweathermap.org/data/2.5/weather?q=Atlanta,us&units=imperial&APPID=1efd23d575e7f6ab1b69c24ba772d747')
+        console.log(weatherData.data)
+        this.setState({
+            weather: weatherData.data
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.weather.name}</h1>
+                <h3>{this.state.weather.main ? Math.round(this.state.weather.main.temp) : null} °F</h3>
+                <h3>{this.state.weather.weather ? this.state.weather.weather[0].description : null}</h3>
+            </div>
+        )
+    }   
+}
+
+export default Weather;
